ce27d1dc807b9492fdeb7c53f45ed73e3caefd15
Duniaalkilany <duniaalkilany908@gmail.com>
2021-04-25 14:55:14 +0300

    lab1

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let username =prompt('what\'s your name ?');
-console.log(username);
+console.log('Hello' + username);
 
 let age =prompt('how old are you ?');
-console.log(age);
+console.log('your old is '+ age);
 
 
 let uni =prompt('Have you studied at the university and what is your university major?');
-console.log(uni);
+console.log('your university major is ' + uni);
 
 let hoppy =prompt('What is your favorite hobby ?');
-console.log(hoppy);
+console.log('your hoppy is ' + hoppy);
 
 alert (' Hello ' + username + ' your old is ' + age +' your university major is ' + uni + ' your hoppy is ' + hoppy);
